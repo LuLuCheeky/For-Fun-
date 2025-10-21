806a4ea9011f2675618f1b63714823fc979492ec
LuLuCheeky <lulucheekypro@gmail.com>
2025-10-21 19:48:39 +0800

    Update JavaThing.js

--- JavaThing.js
+++ JavaThing.js
@@ -1,6 +1,13 @@
 let count = 0;
 
 function incrementCounter() {
-    count++;
-    document.getElementById("counter").innerText = `Button clicked ${count} times`;
+  count++;
+  document.getElementById("counter").innerText = `Button clicked ${count} times`;
+
+  const button = document.querySelector("button");
+  button.classList.add("animate");
+
+  setTimeout(() => {
+    button.classList.remove("animate");
+  }, 300);
 }
